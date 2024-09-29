97e85366619303b0306d7e545317b1b02ed7f795
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2024-09-29 03:52:59 +0200

    .

--- Client/CyclingMotionFE/src/shared/api/route-api.service.ts
+++ Client/CyclingMotionFE/src/shared/api/route-api.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 export interface Direction {
+  lat: number;
+  lng: number;
+}
+
+export interface BrdDto {
   lat: number;
   lng: number;
 }
@@ -16,12 +21,22 @@ export interface GetRouteRequest {
   providedIn: 'root'
 })
 export class RouteApiService {
-  private apiUrl = 'http://localhost:5063/api/Direction'; // Replace with your actual API base URL
+  private apiUrl = 'http://localhost:5063/api'; // Base URL
 
   constructor(private http: HttpClient) { }
 
   getRoutePoints(request: GetRouteRequest): Observable<Direction[]> {
-    return this.http.post<Direction[]>(this.apiUrl, request);
+    return this.http.post<Direction[]>(`${this.apiUrl}/Direction`, request);
+  }
+
+  getAccidents(request: GetRouteRequest): Observable<BrdDto[]> {
+    let params = new HttpParams()
+      .set('StartPoint.Lat', request.startLocation.lat.toString())
+      .set('StartPoint.Lng', request.startLocation.lng.toString())
+      .set('EndPoint.Lat', request.endLocation.lat.toString())
+      .set('EndPoint.Lng', request.endLocation.lng.toString());
+
+    return this.http.get<BrdDto[]>(`${this.apiUrl}/Accidents`, { params });
   }
 }
 
